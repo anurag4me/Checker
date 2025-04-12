eda62e17a862eb6e257c7b21bdab4c1c9718f23a
anurag4me <anuragmishra5033@gmail.com>
2025-04-12 23:01:22 +0530

    Basic server setup using express

---
+++ Backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const PORT = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(PORT, ()=>console.log(`Server started at port:${PORT}`))
